4db2e3e3effff3a917bf0ba453711c3fdd2df46b
FUGA0618 <aax.chiri@gmail.com>
2021-08-08 20:17:38 +0900

    bin/rails g controller Home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
