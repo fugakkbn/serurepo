fae6e68139db5ff99495bdfdb9a525033d020e0e
FUGA0618 <aax.chiri@gmail.com>
2021-08-08 20:44:36 +0900

    TOPページを実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      render :index
+    else
+      render 'welcome/index'
+    end
   end
 end
