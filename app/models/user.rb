9f740595acd3544cc0ecb4b3817d63708cca6250
FUGA0618 <aax.chiri@gmail.com>
2021-08-08 16:16:48 +0900

    deviseで使用するモジュールを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :trackable, :confirmable
 end
