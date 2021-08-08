1165011f83526208028434552f2172a243212653
FUGA0618 <aax.chiri@gmail.com>
2021-08-08 16:46:42 +0900

    letter_opener_webの設定を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
