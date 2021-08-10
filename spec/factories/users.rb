1b66cb321101ecb7d255f38b3533bbc792134a48
FUGA0618 <aax.chiri@gmail.com>
2021-08-10 21:07:59 +0900

    UserのFactoryを定義

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :user do
-    sequence(:email) { |n| "person#{n}@example.com" }
+  factory :alice, class: User do
+    email { 'alice@example.com' }
     password { 'password' }
     password_confirmation { 'password' }
+    confirmed_at { Time.current }
   end
 end
