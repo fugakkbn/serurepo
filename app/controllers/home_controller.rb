d6fb6ef508a90a2a8514f17878546e6f0ddae147
FUGA0618 <aax.chiri@gmail.com>
2021-08-10 21:04:08 +0900

    ログイン前・後用のコントローラを実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
     if user_signed_in?
       render :index
     else
-      render template: 'welcome/index'
+      render 'welcome/index'
     end
   end
 end
