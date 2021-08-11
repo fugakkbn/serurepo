7e8a8c33977c63113fa8ec36491d9a09232bd601
FUGA0618 <aax.chiri@gmail.com>
2021-08-11 09:43:44 +0900

    👮‍

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :alice, class: User do
+  factory :alice, class: 'User' do
     email { 'alice@example.com' }
     password { 'password' }
     password_confirmation { 'password' }
