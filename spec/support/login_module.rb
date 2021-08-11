fa6392aa3e171e14652459e15b99ff658bcffe80
FUGA0618 <aax.chiri@gmail.com>
2021-08-11 16:52:03 +0900

    テストを日本語化に対応

--- spec/support/login_module.rb
+++ spec/support/login_module.rb
@@ -3,8 +3,8 @@
 module LoginModule
   def login(user)
     visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password'
-    click_button 'Log in'
+    fill_in 'Eメール', with: user.email
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
   end
 end
