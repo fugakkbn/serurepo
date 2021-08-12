bb5cf9c7349749f5d47e61dbab36cef467e69611
FUGA0618 <aax.chiri@gmail.com>
2021-08-12 21:20:53 +0900

    bin/rails g controller Books index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'books/index'
   root 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
