4bf228f377ca3f88745de0df611987110399235a
FUGA0618 <aax.chiri@gmail.com>
2021-08-15 18:22:01 +0900

    書籍の検索結果一覧ページのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'books/index'
   root 'home#index'
   devise_for :users
+  resources :books, only: %i[index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
