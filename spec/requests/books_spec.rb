bf63c2697b2637fa1a1e754d0d454040677033d2
FUGA0618 <aax.chiri@gmail.com>
2021-08-15 18:19:33 +0900

    👮‍

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Books", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/books/index"
+RSpec.describe 'Books', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/books/index'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
