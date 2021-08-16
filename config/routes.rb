276f1f008268931cb57d404198e0f95b82e521f3
FUGA0618 <aax.chiri@gmail.com>
2021-08-16 15:01:03 +0900

    users/listsのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :users do
-    get 'list/create'
-    get 'list/show'
-    get 'list/update'
-  end
   root 'home#index'
   devise_for :users
   resources :books, only: %i[index]
+  namespace :users do
+    resources :lists, only: %i[create show update]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
