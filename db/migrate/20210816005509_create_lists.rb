cf2bd8dc03c14ebe2137544cfd50ae61d8f02e85
FUGA0618 <aax.chiri@gmail.com>
2021-08-16 10:02:08 +0900

    Userへのbelongs_toを追加

--- db/migrate/20210816005509_create_lists.rb
+++ db/migrate/20210816005509_create_lists.rb
@@ -1,7 +1,7 @@
 class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
-      t.integer :user_id
+      t.belongs_to :user, index: { unique: true }, foreign_key: true
       t.string :name
 
       t.timestamps
