03d36c6d0391268da911a4e3b4105e032b7b49cc
FUGA0618 <aax.chiri@gmail.com>
2021-08-16 22:46:40 +0900

    👮‍

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,6 @@
 class Book < ApplicationRecord
   has_many :list_details, dependent: :destroy
 
-  validates :isbn_13, presence: true, length: { is: 13 }
+  validates 'isbn_13', presence: true, length: { is: 13 }
   validates :price, presence: true, numericality: { only_integer: true }
 end
