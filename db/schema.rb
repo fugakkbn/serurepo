cbffbad928ed9d15b60d538a8b3dff155904a8f7
FUGA0618 <aax.chiri@gmail.com>
2021-08-16 10:21:29 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_005509) do
+ActiveRecord::Schema.define(version: 2021_08_16_011750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "isbn_13"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.bigint "user_id"
