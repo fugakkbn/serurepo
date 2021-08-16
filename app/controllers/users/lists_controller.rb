67831d4d327c8236dce94ffa2cbbdb3d3448dcd4
FUGA0618 <aax.chiri@gmail.com>
2021-08-16 20:46:06 +0900

    通知リストへの書籍追加機能を実装

--- app/controllers/users/lists_controller.rb
+++ app/controllers/users/lists_controller.rb
@@ -1,9 +1,46 @@
 # frozen_string_literal: true
 
 class Users::ListsController < ApplicationController
-  def create; end
+  def create
+    @book = params[:list][:book]
+    @isbn = @book[:isbn_13]
+    @price = @book[:price]
+
+    if current_user.list.blank?
+      @list = List.new(list_params)
+      @list.save
+    end
+
+    if Book.find_by(isbn_13: @isbn).nil?
+      @item = Book.new(book_params)
+      @item.save
+    else
+      @item = Book.find_by(isbn_13: @isbn)
+    end
+
+    if ListDetail.find_by(list_id: current_user.list.id, book_id: @item.id).nil?
+      @list_detail = ListDetail.new(list_detail_params)
+      @list_detail.save
+    end
+
+    redirect_to request.referer
+  end
 
   def show; end
 
   def update; end
+
+  private
+
+  def list_params
+    params.permit(:user_id, :name).merge(user_id: current_user.id)
+  end
+
+  def book_params
+    params.permit(:isbn_13, :price).merge(isbn_13: @isbn, price: @price)
+  end
+
+  def list_detail_params
+    params.permit(:list_id, :book_id).merge(list_id: current_user.list.id, book_id: @item.id)
+  end
 end
