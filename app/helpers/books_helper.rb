8ed1a971b06b1b6917385b205697c52dc7889ed5
FUGA0618 <aax.chiri@gmail.com>
2021-08-19 19:47:53 +0900

    Userを引数で受け取る形に修正

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module BooksHelper
-  def registered?(isbn)
+  def registered?(isbn, user)
     book = Book.find_by('isbn_13' => isbn)
-    list = current_user.list
+    list = user.list
     list_detail = ListDetail.find_by(list_id: list&.id, book_id: book&.id)
     list_detail.present?
   end
