15612f9142e3590f511504858d0bef0093f10cd6
FUGA0618 <aax.chiri@gmail.com>
2021-08-24 17:23:25 +0900

    ヘッダーメニューのシステムテストを追加

---
+++ spec/system/application_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'application', type: :system do
+  describe 'header menu' do
+    context 'ログインしていない場合' do
+      before do
+        visit root_path
+      end
+
+      it 'サイト名が表示される' do
+        expect(page).to have_selector 'h1', text: 'せるれぽ'
+      end
+
+      it 'ナビゲーションメニューが表示されない' do
+        expect(page).not_to have_selector '.navbar_item'
+      end
+    end
+
+    context 'ログインしてリスト作成済みの場合' do
+      before do
+        list = create(:list)
+        login(list.user)
+        visit root_path
+      end
+
+      it 'ユーザーアイコンが表示される' do
+        expect(page).to have_selector '.material-icons.md-36', text: 'manage_accounts'
+      end
+
+      it 'リストアイコンが表示される' do
+        expect(page).to have_selector '.material-icons.md-36', text: 'receipt_long'
+      end
+    end
+
+    context 'ログインしてリスト未作成の場合' do
+      before do
+        visit_with_auth root_path, :alice
+      end
+
+      it 'ユーザーアイコンが表示される' do
+        expect(page).to have_selector '.material-icons.md-36', text: 'manage_accounts'
+      end
+
+      it 'リストアイコンが表示されない' do
+        expect(page).not_to have_selector '.material-icons.md-36', text: 'receipt_long'
+      end
+    end
+  end
+end
