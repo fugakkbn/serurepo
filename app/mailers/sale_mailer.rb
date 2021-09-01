9e31bf76fc50486cdd47501386f569bf24cf731b
FUGA0618 <aax.chiri@gmail.com>
2021-09-01 19:02:27 +0900

    bin/rails g mailer SaleMailer

---
+++ app/mailers/sale_mailer.rb
@@ -0,0 +1,2 @@
+class SaleMailer < ApplicationMailer
+end
---
+++ spec/mailers/previews/sale_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/sale_mailer
+class SaleMailerPreview < ActionMailer::Preview
+
+end
