d456b7a3b6a56217b2b083ea9a7e9f2e1feb8bae
FUGA0618 <aax.chiri@gmail.com>
2021-09-04 16:29:09 +0900

    RakutenCrawler#runのテストを追加

---
+++ spec/system/rakuten_crawler_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative '../../lib/crawler/rakuten_crawler'
+
+RSpec.describe 'RakutenCrawler', type: :system do
+  describe 'run' do
+    let(:crawler) { RakutenCrawler.new }
+    let(:book) { build(:perfect_rails) }
+
+    it '全ての金額が数値で返ってくること' do # rubocop:disable RSpec/MultipleExpectations
+      crawler.run(book.isbn_13)
+      expect(crawler.single_price).to be_integer
+      expect(crawler.e_book_price).to be_integer
+      expect(crawler.paper_book_price).to be_integer
+    end
+  end
+end
