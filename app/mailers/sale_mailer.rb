bae7a012566757707e71a35a2ec72068e074e504
FUGA0618 <aax.chiri@gmail.com>
2021-09-04 13:55:05 +0900

    SaleMailerクラスを定義

--- app/mailers/sale_mailer.rb
+++ app/mailers/sale_mailer.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class SaleMailer < ApplicationMailer
+  default from: 'aax.chiri@gmail.com'
+
+  def sale_email
+    @user = params[:user]
+    @sale_data = params[:sale_data]
+    @book = Book.find(@sale_data[:book_id])
+    mail(to: @user.email, subject: "【せるれぽ】#{@book.title}がセールになっています")
+  end
 end
