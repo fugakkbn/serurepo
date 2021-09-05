fa80d1bdfe70b780216f27b6fd341d6206e99bc7
FUGA0618 <aax.chiri@gmail.com>
2021-09-05 21:05:57 +0900

    Rake taskの実行を毎日am5:30に実行するように設定

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,7 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+set :environment, :development
+set :output, "#{Rails.root}/log/cron.log"
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
+every 1.day, at: '5:30 am' do
+  rake 'sale_report:start'
+end
