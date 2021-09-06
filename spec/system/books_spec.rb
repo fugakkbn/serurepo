cc1cee8a0596f9fc7a245652aad75f0a182ee365
FUGA0618 <aax.chiri@gmail.com>
2021-09-06 22:34:58 +0900

    ラベルの表示に伴うテストの修正

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'books', type: :system do
     context 'ISBNで検索した場合' do
       it 'プロを目指す人のためのRuby入門が表示される' do
         visit_with_auth root_path, :alice
-        fill_in 'Query', with: '9784774193977'
+        fill_in '検索ワード or ISBN', with: '9784774193977'
         click_button '検索する'
         expect(page).to have_selector '#book-title', text: 'プロを目指す人のためのRuby入門'
       end
@@ -23,7 +23,7 @@ RSpec.describe 'books', type: :system do
     context 'ワード検索した場合' do
       it '.mediaが2つ以上ある' do
         visit_with_auth root_path, :alice
-        fill_in 'Query', with: 'ruby'
+        fill_in '検索ワード or ISBN', with: 'ruby'
         click_button '検索する'
         media = all('.block .media')
         expect(media.size).to be >= 2
@@ -33,7 +33,7 @@ RSpec.describe 'books', type: :system do
     context '検索結果がなかった場合' do
       it '該当する書籍がありませんでしたと表示される' do
         visit_with_auth root_path, :alice
-        fill_in 'Query', with: '123456789'
+        fill_in '検索ワード or ISBN', with: '123456789'
         click_button '検索する'
         expect(page).to have_content '該当する書籍がありませんでした。'
       end
