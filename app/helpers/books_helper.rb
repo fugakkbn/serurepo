d0e754421ad5ab3befe4d88adf11aa9ac41dc385
FUGA0618 <aax.chiri@gmail.com>
2021-09-08 19:11:39 +0900

    BooksHelper#format_priceを追加

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -7,4 +7,12 @@ module BooksHelper
     list_detail = ListDetail.find_by(list_id: list&.id, book_id: book&.id)
     list_detail.present?
   end
+
+  def format_price(price)
+    price = price.to_i
+    return '情報なし' if price.zero?
+
+    price = price.to_s(:delimited)
+    "#{price}円"
+  end
 end
