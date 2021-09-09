6cf50340b776975ae557d6b9d4b021727244ba53
FUGA0618 <aax.chiri@gmail.com>
2021-09-09 16:05:32 +0900

    callback用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :books, only: %i[index]
   resources :list_details, only: %i[destroy]
   namespace :users do
