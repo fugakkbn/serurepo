c45d528110872804afd09f08703e1f121cf02676
FUGA0618 <aax.chiri@gmail.com>
2021-09-10 10:51:08 +0900

    コールバック時の登録処理用のメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[google_oauth2]
 
   has_one :list, dependent: :destroy
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
