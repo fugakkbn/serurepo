5807dde871d19c3dfabfd16e99d8d5cf65ce84ba
FUGA0618 <aax.chiri@gmail.com>
2021-09-10 16:58:43 +0900

    uidのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
 
   has_one :list, dependent: :destroy
 
+  validates :uid, presence: true, uniqueness: { scope: :provider }
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
