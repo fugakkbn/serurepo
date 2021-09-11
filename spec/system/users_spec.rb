b4232594ee2c9198633ccd60d69fb7a9e7e07476
FUGA0618 <aax.chiri@gmail.com>
2021-09-11 19:26:12 +0900

    Google認証ボタンに関するSystemSpecを追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,6 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
+  describe '新規登録画面' do
+    it 'Googleログインボタンが表示されること' do
+      visit new_user_registration_path
+      expect(page).to have_selector "img[src$='google_oauth2_sign_in.png']"
+    end
+  end
+
+  describe 'ログイン画面' do
+    it 'Googleログインボタンが表示されること' do
+      visit new_user_session_path
+      expect(page).to have_selector "img[src$='google_oauth2_sign_in.png']"
+    end
+  end
+
   describe 'アカウント編集' do
     context '通常登録ユーザーの場合' do
       let(:alice) { create(:alice) }
