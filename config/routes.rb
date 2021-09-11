4b14573f3e7b6c6fb29a6f50f02b706cc23ce93b
FUGA0618 <aax.chiri@gmail.com>
2021-09-11 22:49:20 +0900

    利用規約とプライバシーポリシーページへのルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,21 @@
 
 Rails.application.routes.draw do
   root 'home#index'
+
+  get 'tos', to: 'welcome#tos', as: 'tos'
+  get 'privacy_policy', to: 'welcome#privacy_policy', as: 'privacy_policy'
+
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
-  resources :books, only: %i[index]
-  resources :list_details, only: %i[destroy]
+
   namespace :users do
     resources :lists, only: %i[create show]
   end
+
+  resources :books, only: %i[index]
+  resources :list_details, only: %i[destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
