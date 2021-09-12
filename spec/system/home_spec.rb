ae519f3d6aa81e675884c746731a2c3ea9f0a110
FUGA0618 <aax.chiri@gmail.com>
2021-09-12 11:46:52 +0900

    システムテストを追加

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -9,15 +9,26 @@ RSpec.describe 'home', type: :system do
         visit root_path
         expect(page).to have_selector 'h2', text: "「いつか読みたい本」\n安い時に買いませんか？"
       end
+
+      it '利用規約とプライバシーポリシーのリンクが表示されること' do
+        visit root_path
+        expect(page).to have_link '利用規約', href: tos_path
+        expect(page).to have_link 'プライバシーポリシー', href: privacy_policy_path
+      end
     end
 
     context 'ログインして/にアクセスした場合' do
-      let(:alice) { create(:alice) }
+      before do
+        visit_with_auth '/', :alice
+      end
 
       it 'home#indexを表示' do
-        login(alice)
-        visit root_path
         expect(page).to have_selector 'h2', text: '書籍検索'
+      end
+
+      it '利用規約とプライバシーポリシーのリンクが表示されること' do
+        expect(page).to have_link '利用規約', href: tos_path
+        expect(page).to have_link 'プライバシーポリシー', href: privacy_policy_path
       end
     end
   end
