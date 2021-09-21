4c23c28116c75778ee2df1e59e2fdf23924253c1
FUGA0618 <aax.chiri@gmail.com>
2021-09-21 22:45:33 +0900

    ページタイトルを動的に出力するヘルパーを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'せるれぽ'
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
