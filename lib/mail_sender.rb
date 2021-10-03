208dd743def853fba291c243f7b52c7996f7fa63
FUGA0618 <aax.chiri@gmail.com>
2021-10-03 19:55:16 +0900

    ユーザーの割引率によって送信可非を決める処理を追加

--- lib/mail_sender.rb
+++ lib/mail_sender.rb
@@ -3,14 +3,55 @@
 class MailSender
   def self.sale_report(compared_data)
     compared_data.each do |data|
-      next if data[:amazon].nil? && data[:dmm].nil? && data[:rakuten].nil?
+      next if all_data_nil?(data)
 
       details = ListDetail.where(book_id: data[:book_id])
       details.each do |detail|
         user = detail.list.user
-        sale_data = data
-        SaleMailer.with(user: user, sale_data: sale_data).sale_email.deliver_now
+        discount_rating = get_rating(user)
+        book = Book.find(detail.book_id)
+        discounted_price = (book.price * discount_rating).truncate
+
+        data[:amazon] = nil if higher_than_discounted_price?(data[:amazon], discounted_price)
+        data[:dmm] = nil if higher_than_discounted_price?(data[:dmm], discounted_price)
+        data[:rakuten] = nil if higher_than_discounted_price?(data[:rakuten], discounted_price)
+        next if all_data_nil?(data)
+
+        SaleMailer.with(user: user, sale_data: data).sale_email.deliver_now
       end
+    end
+  end
+
+  def self.all_data_nil?(data)
+    if data[:amazon].nil? && data[:dmm].nil? && data[:rakuten].nil?
+      true
+    else
+      false
+    end
+  end
+
+  def self.get_rating(user)
+    case user.discount_rating.to_a
+    when [:even]
+      1
+    when [:over10]
+      0.9
+    when [:over20]
+      0.8
+    when [:over30]
+      0.7
+    when [:over50]
+      0.5
+    else
+      1
+    end
+  end
+
+  def self.higher_than_discounted_price?(price_data, discounted_price)
+    if price_data.present? && (price_data[:price] >= discounted_price)
+      true
+    else
+      false
     end
   end
 end
