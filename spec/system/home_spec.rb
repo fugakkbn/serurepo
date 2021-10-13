9817355dee9e4cb427aa5837a53aaea11df399f3
FUGA0618 <aax.chiri@gmail.com>
2021-10-13 20:54:06 +0900

    home_specを修正

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'home', type: :system do
     context 'ログインせずに/にアクセスした場合' do
       it 'welcome#indexを表示' do
         visit root_path
-        expect(page).to have_selector 'h2', text: "「いつか読みたい本」\n安い時に買いませんか？"
+        expect(page).to have_selector 'h2', text: "\"いつか読みたい本\"\n安い時に買いませんか？"
       end
 
       it '利用規約とプライバシーポリシーのリンクが表示されること' do
