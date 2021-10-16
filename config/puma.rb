82be332fde29e6aef353af9dfe2e4f10b822f8c4
FUGA0618 <aax.chiri@gmail.com>
2021-10-16 20:19:00 +0900

    bindの記述位置を変更

--- config/puma.rb
+++ config/puma.rb
@@ -18,7 +18,6 @@ worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 # port ENV.fetch('PORT', 3000)
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock" if ENV['RAILS_ENV'] == 'production'
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -44,3 +43,5 @@ pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock" if ENV['RAILS_ENV'] == 'production'
