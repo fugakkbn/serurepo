40652feb76e9661dc4d8079242174488cfdd8c95
FUGA0618 <aax.chiri@gmail.com>
2021-10-20 20:51:01 +0900

    TimezoneをJSTにする設定を追加

--- config/application.rb
+++ config/application.rb
@@ -4,13 +4,10 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Serurepo
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
 
@@ -31,12 +28,7 @@ module Serurepo
       system('bundle exec rubocop --auto-correct-all ' + files.join(' '), exception: true)
     end
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
