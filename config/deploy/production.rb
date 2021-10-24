a470630204ccc08c451a8296c617a9851cb85164
FUGA0618 <aax.chiri@gmail.com>
2021-10-24 22:38:16 +0900

    接続先をIPアドレスに変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'serurepo.com', user: 'serureponia', roles: %w{app db web}
+server '52.196.121.178', user: 'serureponia', roles: %w{app db web}
 
 # role-based syntax
 # ==================
