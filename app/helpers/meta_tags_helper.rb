d0455c599199f5d227daebd6bcd9d0e45b07ead5
FUGA0618 <aax.chiri@gmail.com>
2021-10-30 00:02:10 +0900

    OGP設定を修正

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -9,19 +9,19 @@ module MetaTagsHelper
       description: '「いつか読みたい本」を安いときに。書籍のセール通知サービス。',
       viewport: 'width=device-width, initial-scale=1.0',
       og: {
-        title: :title,
+        title: 'せるれぽ',
         type: 'website',
         site_name: 'serurepo',
-        description: :description,
+        description: '「いつか読みたい本」を安いときに。書籍のセール通知サービス。',
         image: 'https://serurepo.com/ogp/ogp.png',
         url: 'https://serurepo.com'
       },
       twitter: {
         card: 'summary',
         site: '@fuga__ch',
-        description: :description,
+        description: '「いつか読みたい本」を安いときに。書籍のセール通知サービス。',
         image: 'https://serurepo.com/ogp/ogp.png',
-        domain: 'serurepo.com'
+        domain: 'https://serurepo.com'
       }
     }
   end
