bf51ae4529f3e7c7d71b32d6f142006569d6441b
FUGA0618 <aax.chiri@gmail.com>
2021-11-03 13:36:11 +0900

    カラム名を変更するmigrationを定義

--- db/migrate/20211103043039_rename_isbn_13_column_to_isbn13.rb
+++ db/migrate/20211103043039_rename_isbn_13_column_to_isbn13.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class RenameIsbn13ColumnToIsbn13 < ActiveRecord::Migration[6.1]
-  def change; end
+  def change
+    rename_column :books, :isbn_13, :isbn13
+  end
 end
