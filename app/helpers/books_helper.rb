15e9d002c7e00016b23551e5cf9ffe8360f34eb1
FUGA0618 <aax.chiri@gmail.com>
2021-11-03 13:18:46 +0900

    使っていないhelperを削除

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module BooksHelper
-  def registered?(isbn, user)
-    book = Book.find_by(isbn13: isbn)
-    list = user.list
-    list_detail = ListDetail.find_by(list_id: list&.id, book_id: book&.id)
-    list_detail.present?
-  end
-
   def format_price(price)
     price = price.to_i
     return '情報なし' if price.zero?
--- spec/helpers/books_helper_spec.rb
+++ spec/helpers/books_helper_spec.rb
@@ -3,41 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe BooksHelper, type: :helper do
-  describe '#registered?' do
-    context 'ListDetailに登録済みの書籍の場合' do
-      it 'trueが返る' do
-        list_detail = create(:list_detail_one)
-        isbn = list_detail.book.isbn13
-        user = list_detail.list.user
-        expect(helper).to be_registered(isbn, user)
-      end
-    end
-
-    context 'ListDetailに登録されていない書籍の場合' do
-      it 'falseが返る' do
-        isbn = create(:perfect_rails).isbn13
-        user = create(:list).user
-        expect(helper).not_to be_registered(isbn, user)
-      end
-    end
-
-    context 'リスト未作成のユーザーの場合' do
-      it 'falseが返る' do
-        isbn = create(:perfect_rails).isbn13
-        user = create(:alice)
-        expect(helper).not_to be_registered(isbn, user)
-      end
-    end
-
-    context '書籍情報が登録されていない場合' do
-      it 'falseが返る' do
-        isbn = build(:perfect_rails).isbn13
-        user = create(:alice)
-        expect(helper).not_to be_registered(isbn, user)
-      end
-    end
-  end
-
   describe '#format_price' do
     context '数値の場合' do
       it '4桁の場合、カンマ区切りに円が付くこと' do
