b7e53696fe5013dca3c899467098eff64f2d6788
FUGA0618 <aax.chiri@gmail.com>
2021-11-10 20:56:12 +0900

    アクセス先URLのパラメーターをHash形式に修正

--- app/models/rakuten_books_searcher.rb
+++ app/models/rakuten_books_searcher.rb
@@ -2,8 +2,15 @@
 
 class RakutenBooksSearcher
   def initialize(query, page_num, count_per_page)
-    @url = "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&hits=#{count_per_page}&affiliateId=#{Rails.application.credentials.rakuten[:af_id]}&applicationId=#{Rails.application.credentials.rakuten[:app_id]}&page=#{page_num}"
-    @url += /^978[0-9]{10}/.match?(query) ? "&isbn=#{query}" : "&title=#{query}"
+    params = {
+      format: :json,
+      hits: count_per_page,
+      affiliateId: Rails.application.credentials.rakuten[:af_id],
+      applicationId: Rails.application.credentials.rakuten[:app_id],
+      page: page_num,
+      (/^978[0-9]{10}/.match?(query) ? :isbn : :title) => query
+    }.to_query
+    @url = "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?#{params}"
   end
 
   def run
