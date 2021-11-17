971cddc074b2c8d2ebc043c53da9996eea639db2
FUGA0618 <aax.chiri@gmail.com>
2021-11-17 20:20:36 +0900

    URLの生成方法を変更

--- app/models/rakuten_books_searcher.rb
+++ app/models/rakuten_books_searcher.rb
@@ -2,7 +2,8 @@
 
 class RakutenBooksSearcher
   def initialize(query, page_num, count_per_page)
-    params = {
+    uri = URI('https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404')
+    uri.query = {
       format: :json,
       hits: count_per_page,
       affiliateId: Rails.application.credentials.rakuten[:af_id],
@@ -10,7 +11,7 @@ class RakutenBooksSearcher
       page: page_num,
       (/^978[0-9]{10}/.match?(query) ? :isbn : :title) => query
     }.to_query
-    @url = "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?#{params}"
+    @url = uri.to_s
   end
 
   def run
