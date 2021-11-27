09866bbcc5d5f7ecb97da18b1ddcb83abd7d2344
FUGA0618 <aax.chiri@gmail.com>
2021-11-27 21:27:31 +0900

    紙版書籍のインデックス取得方法を動的に変更

--- lib/crawler/amazon_crawler.rb
+++ lib/crawler/amazon_crawler.rb
@@ -29,7 +29,8 @@ class AmazonCrawler < Crawler
       kindle_price = ver_list_dom[0].text.split("\n")[1].delete('￥,').to_i
       data << kindle_price
 
-      paper_price = ver_list_dom[-1].text.split("\n")[1].delete('￥,').to_i
+      paper_price_index = ver_list_dom.size == 2 ? 1 : 2
+      paper_price = ver_list_dom[paper_price_index].text.split("\n")[1].delete('￥,').to_i
       data << paper_price
 
       ver_list_dom[0].click
@@ -38,8 +39,6 @@ class AmazonCrawler < Crawler
       before_target_index = split_url.index('dp')
       data << split_url[before_target_index.next]
     end
-    @kindle_price = data[0]
-    @paper_price = data[1]
-    @asin = data[2]
+    @kindle_price, @paper_price, @asin = data
   end
 end
